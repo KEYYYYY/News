fde3d29424355a813f2edb5d06532137cc6691e8
KEYYYYY <763997136@qq.com>
2017-09-12 14:15:06 +0800

    完成新闻内容的简要展示

--- app/home/views.py
+++ app/home/views.py
@@ -1,12 +1,20 @@
 from flask import Blueprint, render_template, request
 
+from app.home.models import New, Type
+
 home = Blueprint('home', __name__)
 
 
 @home.route('/')
 def index():
-    category = request.args.get('category', 'hot')
-    return render_template('home/index.html', category=category)
+    category_name = request.args.get('category', '生活')
+    category_id = Type.query.filter_by(name=category_name).first().id
+    news = New.query.filter_by(type_id=category_id).all()
+    return render_template(
+        'home/index.html',
+        category=category_name,
+        news=news
+    )
 
 
 @home.route('/news/<int:new_id>')
